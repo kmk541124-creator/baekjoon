6033079fd1915b0759ec61c896467a86ba689ed1
kmk <kmk541124@gmail.com>
2026-04-07 20:54:03 +0900

    수정했음

---
+++ 백준 2579-계단오르기.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class stair{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        if(str == null) return;
+        int num = Integer.parseInt(str);
+        for(int i = 0; i < num; i++){
+            
+        }
+    }
+}
